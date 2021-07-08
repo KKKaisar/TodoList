20e4d9f81a058391ee8c55cdd7f710ac46ff28a1
Kaisar <kaldarkajsar@gmail.com>
2021-07-08 22:46:05 +0600

    Todolist

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TodoList :todos="todos" @remove-todo="removeTodo" />
+  </div>
+</template>
+
+<script>
+import TodoList from "@/components/TodoList";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      todos: ["First", "Second"],
+    };
+  },
+  components: { TodoList },
+  methods: {
+    removeTodo(i) {
+      this.todos.splice(i, 1);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="(todo, i) in todos" @click="rmTodo(i)" :key="i + 1">
+      {{ `${i + 1} ${todo}` }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["todos"],
+  methods: {
+    rmTodo(i) {
+      this.$emit("remove-todo", i);
+    },
+  },
+};
+</script>
